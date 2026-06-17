9bac5c24bfa71033a248dfef4f37e3acc0f1a325
Tomek <marzedat@gmail.com>
2026-06-17 11:04:35 +0200

    Dodano zoom i obsługę zdjęć na urządzeniach mobilnych

--- ars_coloris_front/src/components/ImageModal.js
+++ ars_coloris_front/src/components/ImageModal.js
@@ -1,19 +1,14 @@
+import {
+    TransformWrapper,
+    TransformComponent
+} from "react-zoom-pan-pinch";
+
 function ImageModal({ images, currentIndex, onClose, onNext, onPrevious }) {
     if (!images || images.length === 0 || currentIndex === null) {
         return null;
     }
 
     const currentImage = images[currentIndex];
-
-    const handleMouseMove = (event) => {
-        const image = event.currentTarget;
-        const rect = image.getBoundingClientRect();
-
-        const x = ((event.clientX - rect.left) / rect.width) * 100;
-        const y = ((event.clientY - rect.top) / rect.height) * 100;
-
-        image.style.transformOrigin = `${x}% ${y}%`;
-    };
 
     return (
         <div className="modal-overlay">
@@ -30,12 +25,20 @@ function ImageModal({ images, currentIndex, onClose, onNext, onPrevious }) {
                 </button>
             )}
 
-            <img
-                src={currentImage}
-                alt="Powiększona mozaika"
-                className="modal-image zoomable-image"
-                onMouseMove={handleMouseMove}
-            />
+            <TransformWrapper
+    initialScale={1}
+    minScale={1}
+    maxScale={5}
+    centerOnInit
+>
+    <TransformComponent>
+        <img
+            src={currentImage}
+            alt="Powiększona mozaika"
+            className="modal-image"
+        />
+    </TransformComponent>
+</TransformWrapper>
 
             {images.length > 1 && (
                 <button
