9973d6cab3c4eba3ee52364a8b98326c69dd9b8f
Tomek <marzedat@gmail.com>
2026-06-17 14:28:58 +0200

    poprawa navbar i logo przekierowuje na strone głowna

--- ars_coloris_front/src/components/Navbar.js
+++ ars_coloris_front/src/components/Navbar.js
@@ -2,10 +2,8 @@ import { Link } from "react-router-dom";
 import { useState } from "react";
 
 import logo from "../assets/hero_mosaic_logo.jpg";
-import ImageModal from "./ImageModal";
 
 function Navbar() {
-    const [showLogo, setShowLogo] = useState(false);
     const [menuOpen, setMenuOpen] = useState(false);
 
     const closeMenu = () => {
@@ -13,64 +11,54 @@ function Navbar() {
     };
 
     return (
-        <>
-            <nav className="navbar">
-                <div
-                    className="logo-link"
-                    onClick={() => setShowLogo(true)}
-                >
-                    <img
-                        src={logo}
-                        alt="Ars Coloris"
-                        className="navbar-logo"
-                    />
-                </div>
+        <nav className="navbar">
+            <Link to="/" className="logo-link" onClick={closeMenu}>
+                <img
+                    src={logo}
+                    alt="Ars Coloris"
+                    className="navbar-logo"
+                />
+            </Link>
 
-                <button
-                    className="hamburger"
-                    onClick={() => setMenuOpen(!menuOpen)}
-                >
-                    ☰
-                </button>
+            <button
+                className="hamburger"
+                onClick={() => setMenuOpen(!menuOpen)}
+            >
+                ☰
+            </button>
 
-                <ul className={`nav-links ${menuOpen ? "open" : ""}`}>
-                    <li>
-                        <Link to="/" onClick={closeMenu}>
-                            Strona główna
-                        </Link>
-                    </li>
+            <ul className={`nav-links ${menuOpen ? "open" : ""}`}>
+                <li>
+                    <Link to="/" onClick={closeMenu}>
+                        Strona główna
+                    </Link>
+                </li>
 
-                    <li>
-                        <Link to="/gallery" onClick={closeMenu}>
-                            Galeria
-                        </Link>
-                    </li>
+                <li>
+                    <Link to="/gallery" onClick={closeMenu}>
+                        Galeria
+                    </Link>
+                </li>
 
-                    <li>
-                        <Link to="/cooperation" onClick={closeMenu}>
-                            Współpraca
-                        </Link>
-                    </li>
+                <li>
+                    <Link to="/cooperation" onClick={closeMenu}>
+                        Współpraca
+                    </Link>
+                </li>
 
-                    <li>
-                        <Link to="/about" onClick={closeMenu}>
-                            O artystce
-                        </Link>
-                    </li>
+                <li>
+                    <Link to="/about" onClick={closeMenu}>
+                        O artystce
+                    </Link>
+                </li>
 
-                    <li>
-                        <Link to="/contact" onClick={closeMenu}>
-                            Kontakt
-                        </Link>
-                    </li>
-                </ul>
-            </nav>
-
-            <ImageModal
-                image={showLogo ? logo : null}
-                onClose={() => setShowLogo(false)}
-            />
-        </>
+                <li>
+                    <Link to="/contact" onClick={closeMenu}>
+                        Kontakt
+                    </Link>
+                </li>
+            </ul>
+        </nav>
     );
 }
 
